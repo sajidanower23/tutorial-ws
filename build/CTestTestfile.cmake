22626c392a51218f9c13f787d2d8f9ed17b617a0
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-07-27 01:39:14 +1000

    Add initial pkg

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sajidanower23/tutorial-ws/src
+# Build directory: /home/sajidanower23/tutorial-ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
