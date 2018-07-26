2f4753d08cf0441545a9317b884bd9465204acc5
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-07-27 01:43:54 +1000

    Get compile info

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,6 +8,7 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/sajidanower23/tutorial-ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/sajidanower23/tutorial-ws/devel/lib:$LD_LIBRARY_PATH"
 export PATH="/opt/ros/kinetic/bin:/home/sajidanower23/.sdkman/candidates/kotlin/current/bin:/home/sajidanower23/.nix-profile/bin:/home/sajidanower23/.rvm/gems/ruby-2.5.1/bin:/home/sajidanower23/.rvm/gems/ruby-2.5.1@global/bin:/home/sajidanower23/.rvm/rubies/ruby-2.5.1/bin:/home/sajidanower23/sys161/bin:/home/sajidanower23/sys161/tools/bin:~/.local/bin/:~/.stack:/home/sajidanower23/tutorialspoint/sample/bin:/usr/local/go/bin:/home/sajidanower23/.nix-profile/bin:/home/sajidanower23/.local/share/umake/bin:/home/sajidanower23/bin:/home/sajidanower23/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin:/home/sajidanower23/.rvm/bin:/home/sajidanower23/.rvm/bin:/home/sajidanower23/go/bin"
 export PWD="/home/sajidanower23/tutorial-ws/build"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/sajidanower23/tutorial-ws/devel/share/common-lisp"
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -117,6 +117,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/sajidanower23/tutorial-ws/build/gtest/cmake_install.cmake")
+  include("/home/sajidanower23/tutorial-ws/build/my_pubsub/cmake_install.cmake")
 
 endif()
 
