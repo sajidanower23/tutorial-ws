3b99a4beaea12654f45fd6d66dc0a35e22c64b01
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-07-27 20:26:50 +1000

    Add header for the publisher

---
+++ src/my_pubsub/include/my_pubsub/pub_node.hpp
@@ -0,0 +1,23 @@
+/*
+ * Date Started: 09/03/2017
+ * Original Author: Harry J.E Day
+ * Editors:
+ * ROS Node Name: pub_node
+ * ROS Package: my_pubub
+ * Purpose: To demonstrate a simple publisher node.
+ * This code is released under the MIT  License. Copyright BLUEsat UNSW, 2017
+ */
+
+// These import the main ros libary and the message we are going to send
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
+
+class Pub_Node {
+    public:
+        Pub_Node();
+        void spin();
+
+    private:
+        ros::NodeHandle nh;
+        ros::Publisher pub;
+};
